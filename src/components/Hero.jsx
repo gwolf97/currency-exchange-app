400b5dbdf629d902a9073ca22e683cec57e555e4
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-07-29 18:15:14 -0500

    Styles for converter and switch function added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import { DropdownButton, Dropdown, Card, Col, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import currencies from "../currencies.js"
+import {FormControl, InputLabel, Select, MenuItem} from "@mui/material"
+import CurrencyFlag from "react-currency-flags";
 
 const Hero = () => {
 
     const [haveReq, setHaveReq] = React.useState('USD')
     const [wantReq, setWantReq] = React.useState('EUR')
-    const [amountHave, setAmountHave] = React.useState(100)
+    const [amountHave, setAmountHave] = React.useState(1)
     const [amountWant, setAmountWant] = React.useState("")
+    const [recentWants, setRecentWants] = React.useState([])
     
     React.useEffect(() => {
         const options = {
@@ -20,40 +23,59 @@ const Hero = () => {
         
         fetch(`https://currency-converter-by-api-ninjas.p.rapidapi.com/v1/convertcurrency?have=${haveReq}&want=${wantReq}&amount=${amountHave}`, options)
             .then(response => response.json())
-            .then(response => setAmountWant(response.new_amount))
+            .then(response => {setAmountWant(response.new_amount) ; console.log(response)})
             .catch(err => console.error(err));
+        
+        
     }, [haveReq, wantReq, amountHave])
 
-    const getCurrencies = currencies.map(
+
+    const getCurrenciesMui = currencies.map(
         x => (
-            <Dropdown.Item key={x.currency} eventKey={x.currency}>{x.currency} | {x.name}</Dropdown.Item>
+            <MenuItem key={x.currency} value={x.currency}><span className="currency">{x.currency}</span><span className="name">{x.name}</span> </MenuItem>
         )
     )
     
   return (
     <div>
-        <Card className="d-flex justify-content-center align-items-center">
+        <Card className="d-flex pt-3 px-5 justify-content-center align-items-center">
         <Row>
-            <Col>
-            <DropdownButton
-      title={haveReq}
-      id="haveReq"
-      onSelect={(e) => setHaveReq(e)}
-    >
-        {getCurrencies}
-    </DropdownButton>
-    <input className="form-control form-control-lg" type="text" onChange={(e) => setAmountHave(e.target.value ? e.target.value : 1)} placeholder={1}/>
+            <Col xs={12} sm={5}>
+             <FormControl fullWidth>
+                <InputLabel id="have">{<CurrencyFlag currency={haveReq} size="lg"/>}</InputLabel>
+                <Select
+                    labelId="have"
+                    id="have"
+                    label="have"
+                    value={haveReq}
+                    onChange={(e) => setHaveReq(e.target.value)}
+                >
+                    {getCurrenciesMui}
+                </Select>
+            </FormControl>
+    <input className="form-control my-3 form-control-lg" type="text" onChange={(e) =>  setAmountHave(e.target.value ? e.target.value : 1)} placeholder={1}/>
         </Col>
-        <Col>
-            <DropdownButton
-      title={wantReq}
-      id="wantReq"
-      onSelect={(e) => setWantReq(e)}
-    >
-        {getCurrencies}
-    </DropdownButton>
-    <input className="form-control form-control-lg" type="text" value={amountWant}/>
-    </Col>
+        <Col className="d-flex justify-content-center align-items-center" xs={12} sm={2}>
+        <i onClick={() => {setHaveReq(wantReq) ; setWantReq(haveReq)}} className="fa-solid fa-repeat" style={{fontSize:"30px", margin:"0 10px 20px 10px", cursor:"pointer"}}></i>
+        </Col>
+        <Col xs={12} sm={5}>
+        <FormControl fullWidth>
+            <InputLabel id="want"><CurrencyFlag currency={wantReq} size="lg"/></InputLabel>
+            <Select
+                labelId="want"
+                id="want"
+                label="want"
+                value={wantReq}
+                onChange={(e) => setWantReq(e.target.value)}
+            >
+                <MenuItem disabled={true} divider={true}>Recent</MenuItem>   
+
+                <MenuItem disabled={true} divider={true}></MenuItem>   
+                {getCurrenciesMui}
+            </Select>
+        </FormControl>
+    <input className="form-control my-3 form-control-lg" type="text" onChange={() =>{}} value={amountWant}/>
+        </Col>
          </Row>
         </Card>
     </div>
