76bbe9da1a1e1ecafc7f28baea5992d57843258b
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-08-04 16:03:55 -0500

    Disclaimer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from "./components/Header"
 import HomePage from "./components/HomePage"
+import Footer from "./components/Footer"
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header/>
       <HomePage/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Footer = () => <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div style={{color:"grey", opacity:"0.7", borderTop:"1px solid lightgrey"}} className="py-5 col-xs-12">
+                <h5 className="text-uppercase">Disclaimer</h5>
+                <p>Please note that there may be up to a 1-hour delay in fetching the latest exchange rates, so we advise against using this application for anything that requires real-time exchange rates.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+export default Footer
